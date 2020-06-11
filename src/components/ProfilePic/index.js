30f1baf98c3dbd7cb06558b15f3e48d73bc02192
Gabriel Barbosa <gabriel.amorim637@gmail.com>
2020-06-10 22:16:39 -0300

    Profile pic default

--- src/components/ProfilePic/index.js
+++ src/components/ProfilePic/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {metrics} from '../../styles/Metrics';
+import {colors} from '../../styles/Colors';
+import {MaterialCommunityIcons as Icon} from '../../styles/LoadFonts';
 import {ProfileBox, ProfileButton, Title, SubTitle} from './styles';
 
-function ProfilePic({name, author, handler}) {
+function ProfilePic({name, author, handler, image}) {
   return (
     <ProfileBox>
-      <ProfileButton onPress={handler}></ProfileButton>
+      <ProfileButton onPress={handler}>
+        <Icon
+          name="account-circle"
+          size={metrics.widthPercentageToDP(120)}
+          color={colors.primary}
+          onPress={null}
+        />
+      </ProfileButton>
     </ProfileBox>
   );
 }

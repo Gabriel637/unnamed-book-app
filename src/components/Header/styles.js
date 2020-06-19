5463700e215c6b66168e71d06f4c581909ed92d5
Gabriel Amorim Barbosa <37639798+Gabriel637@users.noreply.github.com>
2020-06-19 05:19:47 -0300

    Logo in header position in android

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,13 +2,20 @@ import { metrics } from '../../styles/Metrics';
 import { colors } from '../../styles/Colors';
 import { darkMode } from '../../utils/settingsApp';
 import styled, { css } from 'styled-components/native';
+import { Platform } from 'react-native';
 
 const dark = darkMode();
 
 export const HeaderBox = styled.View`
-  padding: ${metrics.widthPercentageToDP(30)}px
-    ${metrics.widthPercentageToDP(20)}px ${metrics.widthPercentageToDP(0)}px
+  padding: 
+    ${metrics.widthPercentageToDP(30)}px
+    ${metrics.widthPercentageToDP(20)}px 
+    ${metrics.widthPercentageToDP(0)}px
     ${metrics.widthPercentageToDP(20)}px;
+  ${Platform.OS === 'Android' &&
+  css`
+      padding-top: ${metrics.widthPercentageToDP(20)}px;
+    `}
   width: ${metrics.widthPercentageToDP(360)}px;
   height: ${metrics.widthPercentageToDP(70)}px;
   ${dark &&
